73198b4ada47d3042457e6d4987ed50773092f9e
Jayce Azua <jayce.azua@gmail.com>
2018-09-14 00:49:10 -0700

    doing my own shit

--- server.js
+++ server.js
@@ -1,33 +1,50 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const genres = require('./seedData');
+
+const shows = [
+   {
+       id: "1",
+       title: "Mr. Robot",
+       genre: "1",
+       genreTitle: "technology drama",
+       episodes: [
+           "pilot",
+           "hackathon"
+       ]
+   },
+   {
+     id: "2",
+     title: "Rick and Morty",
+     genre: "2",
+     genreTitle: "comedy",
+     episodes: [
+       "pilot",
+       "morty dies"
+     ]
+   }
+ ]
+
 
 app.get('/', (req, res) =>{
-    res.json(genres)
+    res.json(shows)
 });
 
-app.get('/:genre', (req, res, next) =>{
-    let genre = req.params.genre;
-    res.json(genres[genre].title)
-});
+app.get('/:id', (req, res, next) =>{
+    let _id = req.params.id;
+    for (var i = 0; i < shows.length; i++) {
+      if (_id === shows[i].id) {
+        res.json(shows[i])
+      }
 
-app.get('/shows/episodes/:genre', (req, res) =>{
-    let genre = req.params.genre;
-    let showList = [];
-
-    shows.map((show) => {
-        if(show.genre == genre){
-            showList.push(show)
-        }
-    });
-    res.json(showList);
+    }
 });
 
 
 
 
 
-app.listen(port, () =. {
+
+app.listen(port, () => {
   console.log(`Server is alive on port ${port}`)
 });
